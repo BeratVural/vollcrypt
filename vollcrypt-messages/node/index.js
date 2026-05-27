a9954fc39f90a61b859874adc0df88489dc0286f
Berat T. Vural <73348721+BeratVural@users.noreply.github.com>
2026-05-27 23:10:30 +0300

    chore: rename package scope to @vollcrypt and bump version to 0.2.0

--- vollcrypt-messages/node/index.js
+++ vollcrypt-messages/node/index.js
@@ -37,7 +37,7 @@ switch (platform) {
           if (localFileExisted) {
             nativeBinding = require('./vollcrypt.android-arm64.node')
           } else {
-            nativeBinding = require('@vollcrypt-messages/node-android-arm64')
+            nativeBinding = require('@vollcrypt/messages-node-android-arm64')
           }
         } catch (e) {
           loadError = e
@@ -49,7 +49,7 @@ switch (platform) {
           if (localFileExisted) {
             nativeBinding = require('./vollcrypt.android-arm-eabi.node')
           } else {
-            nativeBinding = require('@vollcrypt-messages/node-android-arm-eabi')
+            nativeBinding = require('@vollcrypt/messages-node-android-arm-eabi')
           }
         } catch (e) {
           loadError = e
@@ -69,7 +69,7 @@ switch (platform) {
           if (localFileExisted) {
             nativeBinding = require('./vollcrypt.win32-x64-msvc.node')
           } else {
-            nativeBinding = require('@vollcrypt-messages/node-win32-x64-msvc')
+            nativeBinding = require('@vollcrypt/messages-node-win32-x64-msvc')
           }
         } catch (e) {
           loadError = e
@@ -83,7 +83,7 @@ switch (platform) {
           if (localFileExisted) {
             nativeBinding = require('./vollcrypt.win32-ia32-msvc.node')
           } else {
-            nativeBinding = require('@vollcrypt-messages/node-win32-ia32-msvc')
+            nativeBinding = require('@vollcrypt/messages-node-win32-ia32-msvc')
           }
         } catch (e) {
           loadError = e
@@ -97,7 +97,7 @@ switch (platform) {
           if (localFileExisted) {
             nativeBinding = require('./vollcrypt.win32-arm64-msvc.node')
           } else {
-            nativeBinding = require('@vollcrypt-messages/node-win32-arm64-msvc')
+            nativeBinding = require('@vollcrypt/messages-node-win32-arm64-msvc')
           }
         } catch (e) {
           loadError = e
@@ -113,7 +113,7 @@ switch (platform) {
       if (localFileExisted) {
         nativeBinding = require('./vollcrypt.darwin-universal.node')
       } else {
-        nativeBinding = require('@vollcrypt-messages/node-darwin-universal')
+        nativeBinding = require('@vollcrypt/messages-node-darwin-universal')
       }
       break
     } catch {}
@@ -124,7 +124,7 @@ switch (platform) {
           if (localFileExisted) {
             nativeBinding = require('./vollcrypt.darwin-x64.node')
           } else {
-            nativeBinding = require('@vollcrypt-messages/node-darwin-x64')
+            nativeBinding = require('@vollcrypt/messages-node-darwin-x64')
           }
         } catch (e) {
           loadError = e
@@ -138,7 +138,7 @@ switch (platform) {
           if (localFileExisted) {
             nativeBinding = require('./vollcrypt.darwin-arm64.node')
           } else {
-            nativeBinding = require('@vollcrypt-messages/node-darwin-arm64')
+            nativeBinding = require('@vollcrypt/messages-node-darwin-arm64')
           }
         } catch (e) {
           loadError = e
@@ -157,7 +157,7 @@ switch (platform) {
       if (localFileExisted) {
         nativeBinding = require('./vollcrypt.freebsd-x64.node')
       } else {
-        nativeBinding = require('@vollcrypt-messages/node-freebsd-x64')
+        nativeBinding = require('@vollcrypt/messages-node-freebsd-x64')
       }
     } catch (e) {
       loadError = e
@@ -174,7 +174,7 @@ switch (platform) {
             if (localFileExisted) {
               nativeBinding = require('./vollcrypt.linux-x64-musl.node')
             } else {
-              nativeBinding = require('@vollcrypt-messages/node-linux-x64-musl')
+              nativeBinding = require('@vollcrypt/messages-node-linux-x64-musl')
             }
           } catch (e) {
             loadError = e
@@ -187,7 +187,7 @@ switch (platform) {
             if (localFileExisted) {
               nativeBinding = require('./vollcrypt.linux-x64-gnu.node')
             } else {
-              nativeBinding = require('@vollcrypt-messages/node-linux-x64-gnu')
+              nativeBinding = require('@vollcrypt/messages-node-linux-x64-gnu')
             }
           } catch (e) {
             loadError = e
@@ -203,7 +203,7 @@ switch (platform) {
             if (localFileExisted) {
               nativeBinding = require('./vollcrypt.linux-arm64-musl.node')
             } else {
-              nativeBinding = require('@vollcrypt-messages/node-linux-arm64-musl')
+              nativeBinding = require('@vollcrypt/messages-node-linux-arm64-musl')
             }
           } catch (e) {
             loadError = e
@@ -216,7 +216,7 @@ switch (platform) {
             if (localFileExisted) {
               nativeBinding = require('./vollcrypt.linux-arm64-gnu.node')
             } else {
-              nativeBinding = require('@vollcrypt-messages/node-linux-arm64-gnu')
+              nativeBinding = require('@vollcrypt/messages-node-linux-arm64-gnu')
             }
           } catch (e) {
             loadError = e
@@ -232,7 +232,7 @@ switch (platform) {
             if (localFileExisted) {
               nativeBinding = require('./vollcrypt.linux-arm-musleabihf.node')
             } else {
-              nativeBinding = require('@vollcrypt-messages/node-linux-arm-musleabihf')
+              nativeBinding = require('@vollcrypt/messages-node-linux-arm-musleabihf')
             }
           } catch (e) {
             loadError = e
@@ -245,7 +245,7 @@ switch (platform) {
             if (localFileExisted) {
               nativeBinding = require('./vollcrypt.linux-arm-gnueabihf.node')
             } else {
-              nativeBinding = require('@vollcrypt-messages/node-linux-arm-gnueabihf')
+              nativeBinding = require('@vollcrypt/messages-node-linux-arm-gnueabihf')
             }
           } catch (e) {
             loadError = e
@@ -261,7 +261,7 @@ switch (platform) {
             if (localFileExisted) {
               nativeBinding = require('./vollcrypt.linux-riscv64-musl.node')
             } else {
-              nativeBinding = require('@vollcrypt-messages/node-linux-riscv64-musl')
+              nativeBinding = require('@vollcrypt/messages-node-linux-riscv64-musl')
             }
           } catch (e) {
             loadError = e
@@ -274,7 +274,7 @@ switch (platform) {
             if (localFileExisted) {
               nativeBinding = require('./vollcrypt.linux-riscv64-gnu.node')
             } else {
-              nativeBinding = require('@vollcrypt-messages/node-linux-riscv64-gnu')
+              nativeBinding = require('@vollcrypt/messages-node-linux-riscv64-gnu')
             }
           } catch (e) {
             loadError = e
@@ -289,7 +289,7 @@ switch (platform) {
           if (localFileExisted) {
             nativeBinding = require('./vollcrypt.linux-s390x-gnu.node')
           } else {
-            nativeBinding = require('@vollcrypt-messages/node-linux-s390x-gnu')
+            nativeBinding = require('@vollcrypt/messages-node-linux-s390x-gnu')
           }
         } catch (e) {
           loadError = e
